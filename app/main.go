72590dd7f99aafa5f6ce70528d1cefab3e883843
Scott Burlovich <teedot@gmail.com>
2024-04-14 00:01:34 -0400

    Stage 3

--- app/main.go
+++ app/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"log"
+	"errors"
+	"fmt"
 	"os"
 	"os/exec"
 	// Uncomment this block to pass the first stage!
@@ -13,15 +14,37 @@ import (
 func main() {
 	commandName := os.Args[3]
 	args := os.Args[4:]
-	runCommand(commandName, args)
+	exitCode, err := runCommand(commandName, args)
+	if err != nil {
+		fmt.Printf("Error running command: %v\n", err)
+	}
+
+	os.Exit(exitCode)
 }
 
-func runCommand(commandName string, args []string) {
+func runCommand(commandName string, args []string) (int, error) {
 	command := exec.Command(commandName, args...)
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
 
-	if err := command.Run(); err != nil {
-		log.Fatalf("Error: %v", err)
+	err := command.Start()
+	if err != nil {
+		return 1, err
+	}
+	err = command.Wait()
+	if err != nil {
+		return determineExitCode(err), err
+	}
+
+	return 0, nil
+}
+
+func determineExitCode(err error) int {
+	var exitError *exec.ExitError
+
+	if errors.As(err, &exitError) {
+		return exitError.ExitCode()
+	} else {
+		return 1
 	}
 }
